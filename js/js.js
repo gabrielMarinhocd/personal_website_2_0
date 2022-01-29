5022464bebc37d2ea25bfe9b1db30f8fcbc6d6fd
Gabriel <biel32771@gmail.com>
2022-01-29 12:02:22 -0300

    Criando função para abrir box-eucation

--- js/js.js
+++ js/js.js
@@ -1,1 +1,18 @@
-console.log("Hellow World");
+const education = document.querySelector('#ced-06');
+console.log(education);
+
+education.addEventListener('click', () =>{
+    const iten = document.querySelector(".box-content");
+    let event = iten.style.display;
+    if (event == 'none'){
+        iten.style.display = "block";
+    }else{
+        iten.style.display = "none";
+    }
+    
+    console.log( iten.style.display );
+})
+
+// const openEducation = () =>{
+//     education.style.display = 'none';
+// }
